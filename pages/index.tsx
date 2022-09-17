8b26b94f3a5ef942c412a62799ea5fd38fc87040
Lúcás C. Meier <lucas@cronokirby.com>
2022-09-17 14:25:41 +0200

    Add talk list page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,26 @@
-function MyApp() {
-  return <h1 className="text-red-400">woah mama</h1>
+import { promises as fs } from 'fs';
+import path from 'path';
+
+function MyApp(props: { talks: string[][] }) {
+  return <ul className="text-xxs">
+    {props.talks.map((talk) => <li> <a href={`/talks/${talk[0]}/${talk[1]}/${talk[2]}`}>{talk[talk.length - 1]}</a></li>)}
+  </ul>;
 }
 
 export default MyApp;
 
+
+export async function getStaticProps() {
+  const talks: string[][] = [];
+
+  const root = path.join(process.cwd(), 'talks');
+  for (const year of await fs.readdir(root)) {
+    for (const month of await fs.readdir(path.join(root, year))) {
+      for (const leaf of await fs.readdir(path.join(root, year, month))) {
+        talks.push([year, month, path.parse(leaf).name]);
+      }
+    }
+  }
+  console.log(talks)
+  return { props: { talks } }
+}
