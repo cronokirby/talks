28758ce321f4ce0230845ef8578a139e1fa3d065
Lúcás C. Meier <lucas@cronokirby.com>
2022-09-17 04:27:01 +0200

    Remove vertical gap for textsldie

--- components/TextSlide.tsx
+++ components/TextSlide.tsx
@@ -11,7 +11,7 @@ interface TextSlideProps {
 
 export default function TextSlide(props: TextSlideProps) {
     return <Background bg={props.bg}>
-        <div className="flex flex-col gap-y-4 py-1 px-2 h-full">
+        <div className="flex flex-col py-1 px-2 h-full">
             <Header />
             <div className={`flex-grow w-7/12 mx-auto ${props.size === 'vsmall'? 'text-xxs' : props.size === 'small' ? 'text-xs' : ''}`}>
                 {props.children}
