90e6898578379c71890c2679b7ba7cdb3197f1a7
Lúcás C. Meier <lucas@cronokirby.com>
2022-09-17 04:20:11 +0200

    Add math support

--- pages/[talk].tsx
+++ pages/[talk].tsx
@@ -2,11 +2,15 @@ import { promises as fs } from 'fs';
 import { serialize } from 'next-mdx-remote/serialize';
 import { MDXRemote } from 'next-mdx-remote';
 import path from 'path';
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+
 
 import Section from '../components/Section';
 import TextSlide from '../components/TextSlide';
 import Talk from '../components/Talk';
 import Splash from '../components/Splash';
+import React from 'react';
 
 
 function Ul(props: any) {
@@ -17,8 +21,13 @@ function H1(props: any) {
     return <h1 className="text-2xl font-semibold mb-4" {...props}>{props.children}</h1>
 }
 
+function Annotation(props: any) {
+    return <React.Fragment />
+}
+
 const components = {
     h1: H1,
+    annotation: Annotation,
     ul: Ul,
     Talk,
     Section,
@@ -34,7 +43,12 @@ export default function Post({ source }: any) {
 
 export async function getStaticProps({ params }: any) {
     const source = await fs.readFile(path.join(process.cwd(), `talks/${params.talk}.mdx`));
-    const mdxSource = await serialize(source.toString());
+    const mdxSource = await serialize(source.toString(), {
+        mdxOptions: {
+            remarkPlugins: [remarkMath],
+            rehypePlugins: [rehypeKatex],
+        }
+    });
     return { props: { source: mdxSource } };
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
         <Html>
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&family=IBM+Plex+Serif:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"></link>
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
+                    integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
+                    crossOrigin="anonymous"
+                />
             </Head>
             <body>
                 <Main />
